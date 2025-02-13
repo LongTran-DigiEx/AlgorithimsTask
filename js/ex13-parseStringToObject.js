e07357be4b26b3faaa4dbfd0e24478d793198ec9
LongTran-DigiEx <114660083+m00nLeo@users.noreply.github.com>
2025-02-13 10:44:57 +0700

    algorithims pt.3

--- js/ex13-parseStringToObject.js
+++ js/ex13-parseStringToObject.js
@@ -1,8 +1,20 @@
+const readline = require("readline");
+
+let rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.question("Enter a string: ", (value) => {
+  parseStringToObject(value);
+  rl.close();
+});
+
 function parseStringToObject(str) {
   const obj = {};
   let key = "";
   let value = `'`;
   let isKey = true;
+  let subKey1 = "";
+  let subKey2 = "";
+  let countBraces = 0;
 
   for (let i = 0; i < str.length; i++) {
     const character = str[i];
@@ -10,28 +22,75 @@ function parseStringToObject(str) {
     if (character === ":") {
       isKey = false;
     } else if (character === "{") {
-      if (str[i + 1] === "{") {
-      } else if (str[i + 1] === ",") {
-      }
-    } else if (character === "}") {
+      countBraces++;
       key = key.trim();
-      obj[key] = value;
+
+      if (subKey1) {
+        subKey2 = key;
+        obj[subKey1][subKey2] = {};
+      } else {
+        subKey1 = key;
+        obj[subKey1] = {};
+      }
+
+      key = "";
+      value = `'`;
+      isKey = true;
+    } else if (character === "}") {
+      countBraces--;
+      isKey = true;
+      key = key.trim();
+      value += `'`;
+
+      if (subKey1 && !subKey2) {
+        if (typeof obj[subKey1] === "object" && obj[subKey1] !== null) {
+          obj[subKey1][key] = value;
+        }
+      } else if (subKey1 && subKey2) {
+        if (
+          typeof obj[subKey1][subKey2] === "object" &&
+          obj[subKey1][subKey2] !== null
+        ) {
+          obj[subKey1][subKey2][key] = value;
+        }
+      } else {
+        obj[key] = value;
+      }
+      if (countBraces === 1) {
+        subKey2 = "";
+      }
+      if (countBraces === 0) {
+        subKey1 = "";
+      }
+      key = "";
+      value = `'`;
     } else if (character === ",") {
       isKey = true;
       value += `'`;
       key = key.trim();
-      obj[key] = value;
+
+      if (typeof obj[subKey1] === "object" && obj[subKey1] !== null) {
+        obj[subKey1][key] = value;
+      } else {
+        obj[key] = value;
+      }
       key = "";
-      value = "";
+      value = `'`;
     } else {
       isKey ? (key += character) : (value += character);
     }
   }
-  return obj;
-}
+  isKey = true;
+  key = key.trim();
+  value += `'`;
 
-console.log(
-  parseStringToObject(
-    "key1:value1, key2:{subkey1:subvalue1, subkey2:subvalue2}, key3:value3"
-  )
-);
+  if (typeof obj[subKey1] === "object" && obj[subKey1] !== null) {
+    obj[subKey1][key] = value;
+  } else {
+    obj[key] = value;
+  }
+  subKey1 = "";
+  key = "";
+  value = `'`;
+  return console.dir(obj);
+}
